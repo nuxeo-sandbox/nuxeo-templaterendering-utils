fa1ac9ce7c6417011cbe7d3d22e68a8507d7781b
Michaël Vachette <5880528+michaelva@users.noreply.github.com>
2024-12-17 18:05:08 +0900

    Upgrade to Java 21 API

--- nuxeo-templaterendering-utils-core/src/test/java/org/nuxeo/labs/templaterendering/utils/TestTemplateRenderingAutomation.java
+++ nuxeo-templaterendering-utils-core/src/test/java/org/nuxeo/labs/templaterendering/utils/TestTemplateRenderingAutomation.java
@@ -24,7 +24,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 
-import javax.inject.Inject;
+import jakarta.inject.Inject;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
